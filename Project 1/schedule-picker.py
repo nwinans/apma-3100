7f93ed6e1ab5bad7926897abd07aeb08c2a86b97
Nick Winans <nw5zp@virginia.edu>
2020-01-23 15:55:37 -0500

    schedule picker

---
+++ Project 1/schedule-picker.py
@@ -0,0 +1,14 @@
+count = 0
+classes = ['calculus', 'physics', 'orgo', 'history', 'literature', 'french', 'astronomy']
+
+for a in classes:
+    for b in classes:
+        if a is not b:
+            for c in classes:
+                if a is not c and b is not c:
+                    for d in classes: 
+                        if a is not d and b is not d and c is not d:
+                            print(a + ", " + b + ", " + c + ", " + d)
+                            count = count + 1
+
+print("There are " + count + " class schedule possibilities")
