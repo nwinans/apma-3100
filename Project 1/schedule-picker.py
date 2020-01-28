f038eae19df5aecc9455439f3456199ca65fa95a
Nick Winans <nw5zp@virginia.edu>
2020-01-28 09:56:10 -0500

    added 1a

--- Project 1/schedule-picker.py
+++ Project 1/schedule-picker.py
@@ -1,3 +1,15 @@
+orders = 0
+books = ['calculus', 'physics', 'chemistry', 'history', 'literature','french', 'astronomy']
+
+for i in range(0, len(books)):
+    for j in range(i + 1, len(books)):
+        for k in range(j + 1, len(books)):
+            for l in range(k + 1, len(books)):
+                for m in range(l + 1, len(books)):
+                    orders += 1
+
+print("There are " + str(orders) + " unique book orders")
+
 count = 0
 classes = ['calculus', 'physics', 'orgo', 'history', 'literature', 'french', 'astronomy']
 
