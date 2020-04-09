76c74a04ec4b831c49994de335e0c547d01f638b
Nick Winans <nw5zp@virginia.edu>
2020-04-09 11:13:00 -0400

    fix plot 1

--- Project 3/project.py
+++ Project 3/project.py
@@ -4,11 +4,11 @@ import math
 
 tau = 57
 a = 1 / tau
-x = np.linspace(0, 100, 1000)
+x = np.linspace(0, 500, 1000)
 
 def f_X (x):
     return a*a*x*math.exp(-a*a*x*x/2)
 
 fX = np.vectorize(f_X)
-plt.plot(x, fX)
+plt.plot(x, fX(x))
 plt.show()
