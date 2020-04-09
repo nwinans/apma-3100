3b1d26c191a6c9ff739ea830adf7f1e50eaac196
Nick Winans <nw5zp@virginia.edu>
2020-04-09 11:15:05 -0400

    3.2

--- Project 3/project.py
+++ Project 3/project.py
@@ -4,11 +4,17 @@ import math
 
 tau = 57
 a = 1 / tau
-x = np.linspace(0, 500, 1000)
+x = np.linspace(0, 200, 500)
 
 def f_X (x):
     return a*a*x*math.exp(-a*a*x*x/2)
 
+def F_X (x):
+    return 1 - math.exp(-a*a*x*x/2)
+
 fX = np.vectorize(f_X)
+FX = np.vectorize(F_X)
+
 plt.plot(x, fX(x))
+plt.plot(x, FX(x))
 plt.show()
