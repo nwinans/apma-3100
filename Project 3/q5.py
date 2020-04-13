7f97dad2e702029bc551af01f52dc6e488204592
Nick Winans <nw5zp@virginia.edu>
2020-04-13 11:11:01 -0400

    q5 progess

---
+++ Project 3/q5.py
@@ -0,0 +1,10 @@
+from math import sqrt
+
+z_j = [-1.4, -1.0, -0.5, 0, 0.5, 1.0, 1.4]
+
+def transform(m_n_array, mu, sigma, n):
+    z_n = m_n_array
+    for z in z_n:
+        z = (z - mu)/(sigma/sqrt(n))
+
+def estimations:
