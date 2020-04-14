75a6f42fe5df04f47bf9b5bec9efd52fc40f040e
Aldrick Johan <aldrickjohan@gmail.com>
2020-04-14 01:43:03 -0400

    added estimate and evaluate functions for part 5

--- Project 3/q5.py
+++ Project 3/q5.py
@@ -1,10 +1,24 @@
 from math import sqrt
+import scipy.stats as st
 
 z_j = [-1.4, -1.0, -0.5, 0, 0.5, 1.0, 1.4]
 
 def transform(m_n_array, mu, sigma, n):
-    z_n = m_n_array
+    z_n = m_n_array.copy()
     for z in z_n:
         z = (z - mu)/(sigma/sqrt(n))
+    return z_n
 
-#def estimations:
+def estimations(z_n, j):
+    z1 = z_j[j]
+    for z2 in z_n:
+        if z2 >= z1:
+            return z_n.find(z2) / len(z_n)
+
+def evaluateMAD(z_n):
+    mad = []
+    for i in range (0,7):
+        F_n = estimations(z_n, i)
+        phi = st.norm.cdf(z_j[i])
+        mad.append(abs(F_n - phi))
+    return max(mad)
